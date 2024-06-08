862be140545591c32bb9d15fc3eec2ae51f64e85
Schad <vainalaaryan@gmail.com>
2024-06-08 19:05:14 +0530

    Updated for timestamp input and fixed error in plotting

--- assignment_1_goes_time_series.py
+++ assignment_1_goes_time_series.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-06/06/2024
-"""
 import sunpy.timeseries as ts
 from sunpy.net import Fido, attrs as a
 from datetime import datetime, timedelta
@@ -35,24 +29,17 @@ def plot_time_series(time_series, start_time, end_time, highlight_time):
     the timestamp.
     """
     time_series_trunc = time_series.truncate(start_time, end_time)
-    if time_series_trunc:
-        fig, ax = plt.subplots()
+    fig, ax = plt.subplots()
+    time_series_trunc.plot(axes=ax)
+    ax.axvline(highlight_time, color='steelblue', linewidth=1.5, label="t={}".format(highlight_time))
+    ax.legend()
+    plt.show()
 
-        # Plot the time series manually
-        time_series.plot()
-        
-        ax.axvline(highlight_time, color='steelblue', linewidth=1.5, label="t={}".format(highlight_time))
-        ax.legend()
-        
-        plt.tight_layout()
-        plt.show()
-    else:
-        print("No data available for the specified time range.")
 
 def main():
-    timestamp_str = "2017-12-21T15:41:59Z"
+    timestamp_str = str(input("Please enter time stamp: "))
     timestamp = datetime.strptime(timestamp_str, "%Y-%m-%dT%H:%M:%SZ")
-    
+
     time_window_minutes = int(input("Enter a time window in minutes: "))
     if time_window_minutes <= 0:
         raise ValueError("The time window must be a positive integer.")
@@ -60,5 +47,6 @@ def main():
     start_time, end_time = get_time_window(timestamp, time_window_minutes)
     goes_ts = fetch_satellite_data(start_time, end_time)
     plot_time_series(goes_ts, start_time, end_time, timestamp)
+
 
 main()
